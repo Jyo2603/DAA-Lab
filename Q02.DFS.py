342623b52bce054015b094982507660f63ebe738
Jyotsna <138552575+Jyo2603@users.noreply.github.com>
2024-08-03 21:32:17 +0530

    Update Q02.DFS.py

--- Q02.DFS.py
+++ Q02.DFS.py
@@ -1,20 +1,18 @@
-def dfs(matrix, node, visited=None):
-    if visited is None:
-        visited = set()
-    
-    visited.add(node)
+def DFS(arr, visited, node):
+    if visited[node]: return
+    visited[node] = True
     print(node)
+    for neighbor in range(len(arr[node])):
+        if arr[node][neighbor] and not visited[neighbor]:
+            DFS(arr, visited, neighbor)
 
-    for i, adjacent in enumerate(matrix[node]):
-        if adjacent and i not in visited:
-            dfs_matrix(matrix, i, visited)
-    
-    return visited
-
-n = int(input("Enter the number of nodes: "))
-matrix = [list(map(int, input().split())) for _ in range(n)]
-
-start_node = int(input("Enter the starting node for DFS: "))
-
+n = int(input("Enter number of vertices: "))
+arr = [[0] * n for _ in range(n)]
+for _ in range(int(input("Enter number of edges: "))):
+    u, v = map(int, input("Enter edge (u, v): ").split())
+    arr[u][v] = arr[v][u] = 1
+print("Adjacency matrix:")
+for row in arr: print(row)
+start = int(input("Enter start node: "))
 print("DFS traversal:")
-dfs(matrix, start_node)
+DFS(arr, [False] * n, start)
