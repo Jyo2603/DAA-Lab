a00e097f64df4b5c6aa36887f07d4a398eb22359
Jyotsna <138552575+Jyo2603@users.noreply.github.com>
2024-08-06 18:03:46 +0530

    Update Q12.Knapsack.py

--- Q12.Knapsack.py
+++ Q12.Knapsack.py
@@ -1,12 +1,10 @@
 def knapsack(values, weights, capacity):
     n = len(values)
-    dp = [[0 for _ in range(capacity + 1)] for _ in range(n + 1)]
+    dp = [[0] * (capacity + 1) for _ in range(n + 1)]
 
-    for i in range(n + 1):
-        for w in range(capacity + 1):
-            if i == 0 or w == 0:
-                dp[i][w] = 0
-            elif weights[i - 1] <= w:
+    for i in range(1, n + 1):
+        for w in range(1, capacity + 1):
+            if weights[i - 1] <= w:
                 dp[i][w] = max(values[i - 1] + dp[i - 1][w - weights[i - 1]], dp[i - 1][w])
             else:
                 dp[i][w] = dp[i - 1][w]
@@ -16,8 +14,8 @@ def knapsack(values, weights, capacity):
     w = capacity
     for i in range(n, 0, -1):
         if dp[i][w] != dp[i - 1][w]:
-            included_items.append(i - 1)
-            w -= weights[i - 1]
+            included_items.append(i)  # Store the 1-based index of the item
+            w -= weights[i - 1]  # Reduce the capacity by the weight of the included item
 
     return dp[n][capacity], included_items
 
@@ -28,4 +26,4 @@ capacity = int(input("Enter the capacity of the knapsack: "))
 
 result, included_items = knapsack(values, weights, capacity)
 print("The maximum value that can be obtained is:", result)
-print("Items included:", included_items)
+print("Items included (1-based index):", included_items)
