1cff7a9e73dda9d1d5eb0337da981c2a3f80b027
Jyotsna <138552575+Jyo2603@users.noreply.github.com>
2024-08-06 16:43:45 +0530

    Update Q02.DFS.py

--- Q02.DFS.py
+++ Q02.DFS.py
@@ -1,18 +1,25 @@
-def DFS(arr, visited, node):
+def DFS(arr, visited, node, labels):
     if visited[node]: return
     visited[node] = True
-    print(node)
-    for neighbor in range(len(arr[node])):
-        if arr[node][neighbor] and not visited[neighbor]:
-            DFS(arr, visited, neighbor)
+    print(labels[node])
+    for neighbor, connected in enumerate(arr[node]):
+        if connected and not visited[neighbor]:
+            DFS(arr, visited, neighbor, labels)
 
 n = int(input("Enter number of vertices: "))
+labels = [input(f"Enter label for vertex {i}: ") for i in range(n)]
+
 arr = [[0] * n for _ in range(n)]
 for _ in range(int(input("Enter number of edges: "))):
-    u, v = map(int, input("Enter edge (u, v): ").split())
-    arr[u][v] = arr[v][u] = 1
+    u, v = input("Enter edge (u, v): ").split()
+    u_index, v_index = labels.index(u), labels.index(v)
+    arr[u_index][v_index] = arr[v_index][u_index] = 1
+
 print("Adjacency matrix:")
 for row in arr: print(row)
-start = int(input("Enter start node: "))
+
+start_label = input("Enter start node: ")
+start = labels.index(start_label)
+
 print("DFS traversal:")
-DFS(arr, [False] * n, start)
+DFS(arr, [False] * n, start, labels)
