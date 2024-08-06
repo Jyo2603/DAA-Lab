9d51e05a5eacb545d788ae2a1a5c0c008cf208ea
Jyotsna <138552575+Jyo2603@users.noreply.github.com>
2024-08-06 16:13:03 +0530

    Update Q12.Knapsack.py

--- Q12.Knapsack.py
+++ Q12.Knapsack.py
@@ -1,6 +1,7 @@
 def knapsack(values, weights, capacity):
     n = len(values)
     dp = [[0 for _ in range(capacity + 1)] for _ in range(n + 1)]
+
     for i in range(n + 1):
         for w in range(capacity + 1):
             if i == 0 or w == 0:
@@ -9,13 +10,22 @@ def knapsack(values, weights, capacity):
                 dp[i][w] = max(values[i - 1] + dp[i - 1][w - weights[i - 1]], dp[i - 1][w])
             else:
                 dp[i][w] = dp[i - 1][w]
-    return dp[n][capacity]
 
-values = input("Enter the values of the items: ").split()
-values = list(map(int, values))
-weights = input("Enter the weights of the items: ").split()
-weights = list(map(int, weights))
+    # Backtracking to find the included items
+    included_items = []
+    w = capacity
+    for i in range(n, 0, -1):
+        if dp[i][w] != dp[i - 1][w]:
+            included_items.append(i - 1)
+            w -= weights[i - 1]
+
+    return dp[n][capacity], included_items
+
+# Input handling
+values = list(map(int, input("Enter the values of the items: ").split()))
+weights = list(map(int, input("Enter the weights of the items: ").split()))
 capacity = int(input("Enter the capacity of the knapsack: "))
 
-result = knapsack(values, weights, capacity)
+result, included_items = knapsack(values, weights, capacity)
 print("The maximum value that can be obtained is:", result)
+print("Items included:", included_items)
