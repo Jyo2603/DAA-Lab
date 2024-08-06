9cb034ae8e770370535f9841f12be9b61e1fe1fd
Jyotsna <138552575+Jyo2603@users.noreply.github.com>
2024-08-06 17:55:45 +0530

    Update Q05.Counting-Inversion.py

--- Q05.Counting-Inversion.py
+++ Q05.Counting-Inversion.py
@@ -2,45 +2,45 @@ def sort_and_count(L):
     if len(L) <= 1:
         return 0, L
     mid = len(L) // 2
-    left = L[:mid]
-    right = L[mid:]
+    left, right = L[:mid], L[mid:]
     invL, sorted_left = sort_and_count(left)
     invR, sorted_right = sort_and_count(right)
     invS, sorted_list = merge_and_count(sorted_left, sorted_right)
-    total = invL + invR + invS
-    return total, sorted_list
+    return invL + invR + invS, sorted_list
 
 def merge_and_count(A, B):
-    merged = []
-    i = inv = 0
-    j = 0
+    merged, inv, i, j = [], 0, 0, 0
     while i < len(A) and j < len(B):
         if A[i] <= B[j]:
             merged.append(A[i])
             i += 1
         else:
             merged.append(B[j])
-            j += 1
             inv += len(A) - i
+            j += 1
     merged.extend(A[i:])
     merged.extend(B[j:])
     return inv, merged
 
-users = []
-for i in range(3):
-    print(f"Enter song ranking for user {i+1}: ")
-    user = list(map(int, input().split()))
-    users.append(user)
+def main():
+    users = []
+    for i in range(3):
+        print("Enter song ranking for user", i+1, ":")
+        user = list(map(int, input().split()))
+        users.append(user)
+    
+    invB = sort_and_count(users[1])[0]
+    invC = sort_and_count(users[2])[0]
+    
+    print("User 2 has", invB, "inversions.")
+    print("User 3 has", invC, "inversions.")
+    
+    if invC < invB:
+        print("User 3 has similar taste to user 1")
+    elif invB < invC:
+        print("User 2 has similar taste to user 1")
+    else:
+        print("All users have similar taste.")
 
-invC = sort_and_count(users[1])
-invB = sort_and_count(users[2])
-
-print(f"User 2 has {invB[0]} inversions.")
-print(f"User 3 has {invC[0]} inversions.")
-
-if invC[0] < invB[0]:
-    print("User 3 has similar taste to user 1")
-elif invB[0] < invC[0]:
-    print("User 2 has similar taste to user 1")
-else:
-    print("All users have similar taste.")
+if __name__ == "__main__":
+    main()
